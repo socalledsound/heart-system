c7a0a8c473f4c485d0c78c8091f09b5e885ec512
Chris Kubick <socalledsound@gmail.com>
2021-09-29 08:44:06 -0700

    before class

--- index.js
+++ index.js
@@ -1,80 +1,53 @@
-
-const circleSettings= {
-    maxX : 400,
-    maxY : 400,
-    maxR : 50,
-}
-
-const getRandomColor = () => {
-    const r = random(50, 51);
-    const g = random(0, 255);
-    const b = random(100, 255);
-    const a = 10
-    return [r,g,b,a]
-}
-
-const getRandomColor2 = () => {
-    const r = random(50, 51);
-    const g = random(0, 255);
-    const b = random(100, 255);
-    const a = 250
-    return [r,g,b,a]
-}
-
-class RainCircle {
-    constructor(x, y, r, color){
-        this.x = x
-        this.y = y
-        this.r = r
-        this.color = color
-    }
-
-    move(){
-        this.x += random(-1, 1)
-        this.y += random(-1, 1)
-    }
-
-    render(){
-        noStroke();
-        fill(this.color);
-        ellipse(this.x, this.y, this.r);
-    }
-
-    update(){
-        if(this.color[3] > 10){
-            this.color[3]--
-        }
-    }
-}
-
-let circles = []
-const numCircles = 10000
+let circleX;
+let y;
+let circleR;
+let r, g ,b;
 
 function setup() {
     createCanvas(400, 600);
-    //circles = Array.from({length : numCircles}, () => new RainCircle(random(0, circleSettings.maxX), random(0, circleSettings.maxY),random(2, circleSettings.maxR), getRandomColor()))
-
+    r = random(0, 255);
+    g = random(0, 255);
+    b = random(100, 255);
+    circleX = random(0, 400);
+    y = random(0, 600);
+    background(0);
 }
 
 
-function draw() {
-    background(0)
-    circles.push(new RainCircle(random(0, circleSettings.maxX), random(0, circleSettings.maxY),random(2, circleSettings.maxR), getRandomColor2()))
-    circles.forEach( (circle, i) => {
-        circle.move()
-        circle.render()
-        circle.update()
-    
-    })
-}
+circleR = 40;
 
 
 
 
 function mousePressed() {
-   circles.push(new RainCircle(mouseX, mouseY,random(2, circleSettings.maxR), getRandomColor2()))
+    circleR = random(2, 40);
+    g = random(150, 255);
+    r = random (0, 255);
 }
 
+function draw() {
 
+// circle
+    circleX = random(0, 400)
+    y = random(0,600)
+    r = random(50, 51);
+    g = random(0, 255);
+    b = random(100, 255);
+    noStroke();
+    fill(r, g, b, 10);
+    ellipse(circleX, y, circleR);
+    circleR = circleR;
+// eye space
+    //fill(255, 200, 88, 100);
+    //noStroke(50);
+    //rect(50, 100, 225, 100, 100, 100);
+// lines
+    //stroke(0);
+    //strokeWeight(5);
+    //noFill();
+    //arc(80, 50, 550, 850, 0, HALF_PI);
+    //line(0, 50, 400, 50);
+}
 
-
+//function mousePressed() {
+    background(100,250,220);
